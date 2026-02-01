b1a0f3f3bdf90a463ba0a0f597cf573a4212a490
dshioji <dij25.01@gmail.com>
2026-02-01 09:55:56 +0900

    Add --fork option to install.sh for lf_fork binary name

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@ set -e
 
 cd "$(dirname "$0")"
 
+BINARY_NAME="lf"
+
+if [[ "$1" == "--fork" ]]; then
+    BINARY_NAME="lf_fork"
+fi
+
 echo "Building lf..."
 CGO_ENABLED=0 go build -ldflags="-s -w"
 
 mkdir -p ~/bin
 
-echo "Installing lf to ~/bin..."
-cp lf ~/bin/
+echo "Installing as $BINARY_NAME to ~/bin..."
+cp lf ~/bin/$BINARY_NAME
 
 echo "Done. Make sure ~/bin is in your PATH."
